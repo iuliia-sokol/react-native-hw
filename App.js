16563108893cede75eaed55261cb03f56751084c
Iuliia Sokol <avare779@gmail.com>
2023-03-26 16:00:29 +0300

    added bg image

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+
+  const onLogin = () => {
+    console.log(`name: ${name},  password: ${password}`);
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Glory to Ukraine! </Text>
-      <Text>Glory to Heroes!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.bgImage}
+          source={require("./assets/images/bg.jpg")}
+        >
+          <View style={styles.inputsWrapper}>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "ios" ? "padding" : "height"}
+            >
+              <TextInput
+                value={name}
+                onChangeText={nameHandler}
+                placeholder="Username"
+                style={styles.input}
+              />
+              <TextInput
+                value={password}
+                onChangeText={passwordHandler}
+                placeholder="Password"
+                secureTextEntry={true}
+                style={styles.input}
+              />
+              <Button title={"Login"} style={styles.input} onPress={onLogin} />
+            </KeyboardAvoidingView>
+          </View>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+  bgImage: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputsWrapper: {
     alignItems: "center",
     justifyContent: "center",
   },
