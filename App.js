a59ad970d0cc1606da167a5e05051cd564ee7596
Iuliia Sokol <avare779@gmail.com>
2023-03-27 10:48:08 +0300

    added navigation

--- App.js
+++ App.js
@@ -1,36 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./Screens/LoginScreen";
+import Register from "./Screens/RegistrationScreen";
+import Home from "./Screens/Home";
 
 import * as Font from "expo-font";
 // import * as SplashScreen from "expo-splash-screen";
 
 // import { useFonts } from "expo-font";
 
-import {
-  TouchableWithoutFeedback,
-  StyleSheet,
-  View,
-  TextInput,
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  Alert,
-  Pressable,
-  ImageBackground,
-  Text,
-} from "react-native";
-
 const fontsLoaded = {
   "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
   "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
 };
 
-export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [disabled, setDisabled] = useState(true);
-  const [showPassword, setShowPassword] = useState(false);
-  const [showKeyboard, setShowKeyboard] = useState(false);
-  const [focused, setFocused] = useState("");
+const MainStack = createStackNavigator();
+
+export default () => {
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
@@ -41,202 +28,17 @@ export default function App() {
     loadFonts();
   }, []);
 
-  const emailHandler = (text) => {
-    setEmail(text);
-  };
-  const passwordHandler = (text) => {
-    setPassword(text);
-  };
-
-  const onLogin = (e) => {
-    e.preventDefault();
-    console.log({ email: email, password: password });
-    Alert.alert("Credentials", `email: ${email} password: ${password}`);
-    setEmail("");
-    setPassword("");
-  };
-
-  const handleInputShow = () => {
-    setShowPassword(!showPassword);
-  };
-
-  useEffect(() => {
-    if (email && password) {
-      setDisabled(false);
-    }
-    if (!email || !password) {
-      setDisabled(true);
-    }
-  }, [email, password]);
 
   return (
     isReady && (
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.container}>
-          <ImageBackground
-            style={styles.bgImage}
-            source={require("./assets/images/bg.jpg")}
-          >
-            <KeyboardAvoidingView
-              behavior={Platform.OS == "ios" ? "padding" : "height"}
-            >
-              <View
-                style={{
-                  ...styles.form,
-                  paddingBottom: showKeyboard ? 32 : 111,
-                }}
-              >
-                <Text style={styles.title}>Войти</Text>
-                <View style={styles.inputWrapper}>
-                  <TextInput
-                    value={email}
-                    autoFocus={true}
-                    onChangeText={emailHandler}
-                    onFocus={() => {
-                      setShowKeyboard(true);
-                      setFocused("email");
-                    }}
-                    onBlur={() => {
-                      setShowKeyboard(false);
-                      setFocused("");
-                    }}
-                    placeholder="Адрес электронной почты"
-                    style={{
-                      ...styles.input,
-                      borderColor: focused === "email" ? "#FF6C00" : "#E8E8E8",
-                    }}
-                  />
-                </View>
-                <View style={styles.inputWrapper}>
-                  <TextInput
-                    value={password}
-                    onChangeText={passwordHandler}
-                    onFocus={() => {
-                      setShowKeyboard(true);
-                      setFocused("password");
-                    }}
-                    onBlur={() => {
-                      setShowKeyboard(false);
-                      setFocused("");
-                    }}
-                    placeholder="Пароль"
-                    secureTextEntry={!showPassword}
-                    style={{
-                      ...styles.input,
-                      borderColor:
-                        focused === "password" ? "#FF6C00" : "#E8E8E8",
-                    }}
-                  />
-                  <Pressable
-                    style={styles.passwordIndicator}
-                    onPress={handleInputShow}
-                    accessibilityLabel={"Show password"}
-                  >
-                    <Text
-                      style={{
-                        ...styles.passwordIndicatorText,
-                        opacity: !password ? 0.5 : 1,
-                      }}
-                    >
-                      Показать
-                    </Text>
-                  </Pressable>
-                </View>
-
-                <Pressable
-                  disabled={disabled}
-                  style={{ ...styles.button, opacity: disabled ? 0.7 : 1 }}
-                  onPress={onLogin}
-                  accessibilityLabel={"Login"}
-                >
-                  <Text style={styles.buttonText}>Войти</Text>
-                </Pressable>
-
-                <Pressable>
-                  <Text
-                    style={{
-                      ...styles.passwordIndicatorText,
-                      textAlign: "center",
-                      marginTop: 16,
-                    }}
-                  >
-                    Нет аккаунта? Зарегистрироваться
-                  </Text>
-                </Pressable>
-              </View>
-            </KeyboardAvoidingView>
-          </ImageBackground>
-        </View>
-      </TouchableWithoutFeedback>
+      <NavigationContainer>
+         <MainStack.Navigator initialRouteName="Login">
+         <MainStack.Screen name="Registration" component={Register} />
+         <MainStack.Screen name="Login" component={Login} />
+        <MainStack.Screen name="Home" component={Home} />
+         </MainStack.Navigator>
+      </NavigationContainer>
     )
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-  },
-  input: {
-    height: 50,
-    padding: 16,
-    backgroundColor: "#F6F6F6",
-    borderWidth: 1,
-    borderColor: "#E8E8E8",
-    borderRadius: 8,
-    marginBottom: 16,
-  },
-  bgImage: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
-  form: {
-    // height: 489,
-    justifyContent: "flex-start",
-    backgroundColor: "#ffffff",
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    paddingTop: 32,
-    paddingHorizontal: 16,
-  },
-  inputWrapper: {
-    position: "relative",
-  },
-  passwordIndicator: {
-    position: "absolute",
-    right: 16,
-    top: 16,
-  },
-  passwordIndicatorText: {
-    fontFamily: "Roboto-Regular",
-    color: "#1B4371",
-    fontSize: 16,
-    fontWeight: 400,
-    textAlign: "right",
-  },
-
-  title: {
-    fontFamily: "Roboto-Medium",
-    fontSize: 30,
-    color: "#212121",
-    fontWeight: 500,
-    lineHeight: 35,
-    textAlign: "center",
-    letterSpacing: 0.01,
-    marginBottom: 32,
-  },
-
-  button: {
-    backgroundColor: "#FF6C00",
-    borderRadius: 100,
-    padding: 16,
-  },
-  buttonText: {
-    fontFamily: "Roboto-Regular",
-    textAlign: "center",
-    color: "#ffffff",
-  },
-});
---
+++ Screens/LoginScreen.js
@@ -0,0 +1,224 @@
+import React, { useEffect, useState } from "react"; 
+
+import {
+    TouchableWithoutFeedback,
+    StyleSheet,
+    View,
+    TextInput,
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    Alert,
+    Pressable,
+    ImageBackground,
+    Text,
+  } from "react-native";
+
+const Login=({ navigation })=> {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [disabled, setDisabled] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showKeyboard, setShowKeyboard] = useState(false);
+    const [focused, setFocused] = useState("");
+   
+  
+    const emailHandler = (text) => {
+      setEmail(text);
+    };
+    const passwordHandler = (text) => {
+      setPassword(text);
+    };
+  
+    const onLogin = (e) => {
+      e.preventDefault();
+      console.log({ email: email, password: password });
+      Alert.alert("Credentials", `email: ${email} password: ${password}`);
+      setEmail("");
+      setPassword("");
+    };
+  
+    const handleInputShow = () => {
+      setShowPassword(!showPassword);
+    };
+  
+    useEffect(() => {
+      if (email && password) {
+        setDisabled(false);
+      }
+      if (!email || !password) {
+        setDisabled(true);
+      }
+    }, [email, password]);
+  
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <ImageBackground
+              style={styles.bgImage}
+              source={require("../assets/images/bg.jpg")}
+            >
+              <KeyboardAvoidingView
+                behavior={Platform.OS == "ios" ? "padding" : "height"}
+              >
+                <View
+                  style={{
+                    ...styles.form,
+                    paddingBottom: showKeyboard ? 32 : 111,
+                  }}
+                >
+                  <Text style={styles.title}>Log in</Text>
+                  <View style={styles.inputWrapper}>
+                    <TextInput
+                      value={email}
+                      autoFocus={true}
+                      onChangeText={emailHandler}
+                      onFocus={() => {
+                        setShowKeyboard(true);
+                        setFocused("email");
+                      }}
+                      onBlur={() => {
+                        setShowKeyboard(false);
+                        setFocused("");
+                      }}
+                      placeholder="Email address"
+                      style={{
+                        ...styles.input,
+                        borderColor: focused === "email" ? "#FF6C00" : "#E8E8E8",
+                      }}
+                    />
+                  </View>
+                  <View style={styles.inputWrapper}>
+                    <TextInput
+                      value={password}
+                      onChangeText={passwordHandler}
+                      onFocus={() => {
+                        setShowKeyboard(true);
+                        setFocused("password");
+                      }}
+                      onBlur={() => {
+                        setShowKeyboard(false);
+                        setFocused("");
+                      }}
+                      placeholder="Password"
+                      secureTextEntry={!showPassword}
+                      style={{
+                        ...styles.input,
+                        borderColor:
+                          focused === "password" ? "#FF6C00" : "#E8E8E8",
+                      }}
+                    />
+                    <Pressable
+                      style={styles.passwordIndicator}
+                      onPress={handleInputShow}
+                      accessibilityLabel={"Show password"}
+                    >
+                      <Text
+                        style={{
+                          ...styles.passwordIndicatorText,
+                          opacity: !password ? 0.5 : 1,
+                        }}
+                      >
+                        Show
+                      </Text>
+                    </Pressable>
+                  </View>
+  
+                  <Pressable
+                    disabled={disabled}
+                    style={{ ...styles.button, opacity: disabled ? 0.7 : 1 }}
+                    onPress={onLogin}
+                    accessibilityLabel={"Login"}
+                  >
+                    <Text style={styles.buttonText}>Log in</Text>
+                  </Pressable>
+  
+                  <Pressable onPress={() => navigation.navigate("Registration")}>
+                    <Text
+                      style={{
+                        ...styles.passwordIndicatorText,
+                        textAlign: "center",
+                        marginTop: 16,
+                      }}
+                    >
+                      Don't have an account? Register
+                    </Text>
+                  </Pressable>
+                </View>
+              </KeyboardAvoidingView>
+            </ImageBackground>
+          </View>
+        </TouchableWithoutFeedback>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      backgroundColor: "#ecf0f1",
+    },
+    input: {
+      height: 50,
+      padding: 16,
+      backgroundColor: "#F6F6F6",
+      borderWidth: 1,
+      borderColor: "#E8E8E8",
+      borderRadius: 8,
+      marginBottom: 16,
+    },
+    bgImage: {
+      flex: 1,
+      justifyContent: "flex-end",
+    },
+    form: {
+      justifyContent: "flex-start",
+      backgroundColor: "#ffffff",
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+      borderTopLeftRadius: 25,
+      borderTopRightRadius: 25,
+      paddingTop: 32,
+      paddingHorizontal: 16,
+    },
+    inputWrapper: {
+      position: "relative",
+    },
+    passwordIndicator: {
+      position: "absolute",
+      right: 16,
+      top: 16,
+    },
+    passwordIndicatorText: {
+      fontFamily: "Roboto-Regular",
+      color: "#1B4371",
+      fontSize: 16,
+      fontWeight: 400,
+      textAlign: "right",
+    },
+  
+    title: {
+      fontFamily: "Roboto-Medium",
+      fontSize: 30,
+      color: "#212121",
+      fontWeight: 500,
+      lineHeight: 35,
+      textAlign: "center",
+      letterSpacing: 0.01,
+      marginBottom: 32,
+    },
+  
+    button: {
+      backgroundColor: "#FF6C00",
+      borderRadius: 100,
+      padding: 16,
+    },
+    buttonText: {
+      fontFamily: "Roboto-Regular",
+      textAlign: "center",
+      color: "#ffffff",
+    },
+  });
+  
+
+  export default Login;
