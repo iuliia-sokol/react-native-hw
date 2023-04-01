bf2a3f64b26f2ff16709e7078d24199e175e0c2f
Iuliia Sokol <avare779@gmail.com>
2023-04-01 11:58:15 +0300

    started connecting redux

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Login from "./Screens/LoginScreen";
 import Register from "./Screens/RegistrationScreen";
 import Home from "./Screens/Home";
 import useCachedResources from "./hooks/useCachedResourses";
+import { store } from "./redux/store";
 
 
 
--- redux/store.js
+++ redux/store.js
@@ -1,1 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import authSlice from './auth/authSlice';
+
+const rootReducer= combineReducers({
+  auth: authSlice
+})
+
+
+export const store = configureStore({
+    reducer: rootReducer
+})
